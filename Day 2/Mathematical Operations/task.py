e5b748d98a1b1113441c08186795b3ad9fd062c9
Abdi <Abdisaid94@hotmail.com>
2025-11-19 14:29:36 +0100

    First commit

---
+++ Day 2/Mathematical Operations/task.py
@@ -0,0 +1,19 @@
+print("My age: " + str(12))
+print(123 + 456)
+print(7-3)
+print(3 - 2)
+print (3 * 2)
+print (5 / 3)
+print(5 // 3)
+print (2**3)
+
+#PEMDASLR L TO RIGHT (ordningen på det som är viktigast)
+# ()
+# **
+# * OR /
+# + OR -
+
+print (3 * (3 + 3) /3 - 3)
+
+
+
---
+++ Day 2/Number Manipulation/task.py
@@ -0,0 +1,23 @@
+bmi = 84 / 1.65 ** 2
+print (bmi)
+
+print(int(bmi))
+
+print(round(bmi))
+print(round(bmi,2))
+
+
+#user score a point
+score = 0
+score +=1
+print (score)
+
+
+#F-strings
+print ("your score is" + " " + str(score))
+
+score = 0
+height = 1.8
+is_winning = True
+
+print (f"Your score is = {score}, Your height is {height}. You are winning is {is_winning}")
