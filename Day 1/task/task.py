8ab05ab61e6f9784e5c67173ba744f944bec5a00
abdisaid94 <167806809+abdisaid94@users.noreply.github.com>
2025-11-19 15:44:52 +0100

    Delete Day 1 directory

--- Day 1/task/task.py
+++
@@ -1,4 +0,0 @@
-print ("Welcome to the band name generator")
-city = input("What city did you grow up in?\n")
-pet = input("Whats your pets name?\n")
-print("your band name could be " + city + " " + pet)
